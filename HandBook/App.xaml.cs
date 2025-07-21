743181bae78f3383378846f3b036bc2af9e139c7
Bulbul Ahmed <bulbul.ahmed@prothomalo.com>
2025-07-21 12:07:57 +0600

    made changes after downloading zip

---
+++ HandBook/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HandBook
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
